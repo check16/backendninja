a6928efefd247026f0bb0a8d14f632ccf9478d52
Antonio Sanchez <antonio.san.87@gmail.com>
2017-08-17 20:01:43 +0200

    Agregada la funcionalidad de control de log en BD así como un ejemplo de RestController simple.

--- src/main/java/com/asanast/component/RequestTimeInterceptorComponent.java
+++ src/main/java/com/asanast/component/RequestTimeInterceptorComponent.java
@@ -1,17 +1,29 @@
 package com.asanast.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.asanast.repository.LogRepository;
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptorComponent extends HandlerInterceptorAdapter{
 	
 	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptorComponent.class);
+	
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -24,6 +36,14 @@ public class RequestTimeInterceptorComponent extends HandlerInterceptorAdapter{
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long startTime = (long) request.getAttribute("startTime");
+		String url = request.getRequestURL().toString();
+		String username = "";
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if(auth != null && auth.isAuthenticated()) {
+			username = auth.getName();
+		}
+		com.asanast.entity.Log log = new com.asanast.entity.Log(new Date(), auth.getDetails().toString(), username, url);
+		logRepository.save(log);
 		LOG.info("-- REQUEST URL: '" + request.getRequestURL().toString() + "' --TOTAL TIME: '" + (System.currentTimeMillis() - startTime) + "' ms");
 	}
 
